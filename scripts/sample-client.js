135994fecf9cdb99bc6c289eee66a55c9163c0b2
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2021-02-24 15:49:40 +0100

    Fix sample client to work with clsk

--- scripts/sample-client.js
+++ scripts/sample-client.js
@@ -4,16 +4,19 @@ const blockchainNodePort = process.argv[3] || 7001;
 
 (async () => {
 
-  // Address: ldpos313ac2d3d1d081901be0c5ce074d1e81a8a0bf5f
+  // Address: clskbde48efe4fb34f91f3bfad4e4e8feb081b302b0f
   let client = createClient({
     hostname: blockchainNodeIp,
     port: blockchainNodePort,
-    networkSymbol: 'ldpos'
+    networkSymbol: 'clsk',
+    chainModuleName: 'capitalisk_chain'
   });
 
   await client.connect({
     passphrase: 'clerk aware give dog reopen peasant duty cheese tobacco trouble gold angle'
   });
+
+  await client.syncAllKeyIndexes();
 
   // let updateResult = await client.syncAllKeyIndexes();
   // let updateResult = await client.syncKeyIndex('forging');
@@ -21,55 +24,53 @@ const blockchainNodePort = process.argv[3] || 7001;
   // console.log('FORGING KEY INDEX AFTER SYNC', client.forgingKeyIndex);
 
   // Recipient address: imitate forum impose muffin purity harvest area mixed renew orient wife eyebrow
-  for (let i = 0; i < 1; i++) {
-    let preparedTxn = await client.prepareTransaction({
-      type: 'transfer',
-      recipientAddress: 'ldpos75fbb06210575fd8f7f62e0b9267d4386273fc80',
-      amount: `${Math.floor(Math.random() * 1)}000000000`,
-      fee: `10000000`,
-      timestamp: 100000,
-      message: `Test ${i}`
-    });
-  
-    await client.postTransaction(preparedTxn);
-    console.log(`Posted transaction #${i}`);
-  }
+  // let preparedTxn = await client.prepareTransaction({
+  //   type: 'transfer',
+  //   recipientAddress: 'clskefecd5cf611f1e3939b3f2754ad7d5b8ecd620a4',
+  //   amount: '1000000000',
+  //   fee: '10000000',
+  //   timestamp: 100000,
+  //   message: `Test ${i}`
+  // });
+  //
+  // await client.postTransaction(preparedTxn);
+  // console.log(`Posted transaction`);
 
-  let voteTxn = await client.prepareTransaction({
-    type: 'vote',
-    delegateAddress: 'ldpos75fbb06210575fd8f7f62e0b9267d4386273fc80',
-    fee: `20000000`,
-    timestamp: 200000,
-    message: ''
-  });
-  
-  await client.postTransaction(voteTxn);
-  
-  console.log('Prepared transaction:', preparedTxn);
+  // let voteTxn = await client.prepareTransaction({
+  //   type: 'vote',
+  //   delegateAddress: 'clskefecd5cf611f1e3939b3f2754ad7d5b8ecd620a4',
+  //   fee: `20000000`,
+  //   timestamp: Date.now(),
+  //   message: ''
+  // });
+  //
+  // await client.postTransaction(voteTxn);
+  //
+  // console.log('Prepared transaction:', preparedTxn);
+  //
+  // let preparedMultisigTxn = await client.prepareMultisigTransaction({
+  //   type: 'transfer',
+  //   recipientAddress: 'clskefecd5cf611f1e3939b3f2754ad7d5b8ecd620a4',
+  //   amount: `100000000`,
+  //   fee: `10000000`,
+  //   timestamp: Date.now(),
+  //   message: 'Testing...'
+  // });
 
-  let preparedMultisigTxn = await client.prepareMultisigTransaction({
-    type: 'transfer',
-    recipientAddress: 'ldpos75fbb06210575fd8f7f62e0b9267d4386273fc80',
-    amount: `100000000`,
-    fee: `10000000`,
-    timestamp: 100000,
-    message: 'Testing...'
-  });
-  
-  let multisigTxnSignature = await client.signMultisigTransaction(preparedMultisigTxn);
-  console.log('Multisig transaction signature:', multisigTxnSignature);
-
-  let accountList = await client.getAccountsByBalance(0, 10, 'desc');
-  console.log('Account list:', accountList);
-
-  let transactions = await client.getTransactionsByTimestamp(0, 100);
-  console.log('TRANSACTIONS:', transactions);
-
-  let accountVotes = await client.getAccountVotes(client.walletAddress);
-  console.log('ACCOUNT VOTES:', accountVotes);
-
-  let block = await client.getBlockAtHeight(2);
-  console.log('BLOCK:', block);
+  // let multisigTxnSignature = await client.signMultisigTransaction(preparedMultisigTxn);
+  // console.log('Multisig transaction signature:', multisigTxnSignature);
+  //
+  // let accountList = await client.getAccountsByBalance(0, 10, 'desc');
+  // console.log('Account list:', accountList);
+  //
+  // let transactions = await client.getTransactionsByTimestamp(0, 100);
+  // console.log('TRANSACTIONS:', transactions);
+  //
+  // let accountVotes = await client.getAccountVotes(client.walletAddress);
+  // console.log('ACCOUNT VOTES:', accountVotes);
+  //
+  // let block = await client.getBlockAtHeight(2);
+  // console.log('BLOCK:', block);
 
   // let accounts = await client.getAccountsByBalance(0, 100);
   // console.log('ACCOUNTS:', accounts);
