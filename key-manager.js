c50e5c002adfde9e7cc6690e43d9d71cce14b3e5
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2021-12-11 06:02:36 +0100

    Fix lockfile

--- key-manager.js
+++ key-manager.js
@@ -5,32 +5,47 @@ const crypto = require('crypto');
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const unlink = util.promisify(fs.unlink);
-const lockfile = require('proper-lockfile');
+const lockfile = require('lockfile');
+const lock = util.promisify(lockfile.lock);
+const unlock = util.promisify(lockfile.unlock);
 
 const DEFAULT_KEY_INDEX_DIR_PATH = path.resolve(__dirname, 'data');
 const DEFAULT_KEY_INDEX_FILE_EXTENSION = '';
+const DEFAULT_LOCK_FILE_EXTENSION = '.lock';
+const DEFAULT_LOCK_WAIT = 3000;
+const DEFAULT_LOCK_RETRIES = 20;
+const DEFAULT_LOCK_RETRY_WAIT = 100;
+const DEFAULT_LOCK_POLL_PERIOD = 100;
+const DEFAULT_LOCK_STALE = 5000;
 
 class KeyManager {
   constructor(options) {
     this.keyIndexDirPath = options.keyIndexDirPath == null ? DEFAULT_KEY_INDEX_DIR_PATH : options.keyIndexDirPath;
     this.keyIndexFileExtension = options.keyIndexFileExtension == null ? DEFAULT_KEY_INDEX_FILE_EXTENSION : options.keyIndexFileExtension;
-    this.keyIndexFileLockOptions = {
-      onCompromised: () => {},
-      ...options.keyIndexFileLockOptions
+    let { lockFileExtension, ...lockOptions } = options.keyIndexFileLockOptions || {};
+    this.lockFileExtension = lockFileExtension || DEFAULT_LOCK_FILE_EXTENSION;
+    this.lockOptions = {
+      wait: DEFAULT_LOCK_WAIT,
+      retries: DEFAULT_LOCK_RETRIES,
+      retryWait: DEFAULT_LOCK_RETRY_WAIT,
+      pollPeriod: DEFAULT_LOCK_POLL_PERIOD,
+      stale: DEFAULT_LOCK_STALE,
+      ...lockOptions
     };
   }
 
   async lockFile(targetFilePath) {
     try {
-      return await lockfile.lock(targetFilePath, this.keyIndexFileLockOptions);
+      return await lock(`${targetFilePath}${this.lockFileExtension}`, {...this.lockOptions});
     } catch (error) {
-      if (error.code === 'ENOENT') {
-        return () => Promise.resolve();
-      }
       throw new Error(
         `Failed to lock the key index file ${targetFilePath} because of error: ${error.message}`
       );
     }
+  }
+
+  async unlockFile(targetFilePath) {
+    return unlock(`${targetFilePath}${this.lockFileExtension}`);
   }
 
   getKeyFilePath(key) {
@@ -40,43 +55,43 @@ class KeyManager {
 
   async incrementKeyIndex(key) {
     let targetFilePath = this.getKeyFilePath(key);
-    let release = await this.lockFile(targetFilePath);
+    await this.lockFile(targetFilePath);
     let keyIndex = await this._loadKeyIndexFromFile(targetFilePath, key);
     await this._saveKeyIndexToFile(targetFilePath, key, ++keyIndex);
-    await release();
+    await this.unlockFile(targetFilePath);
     return keyIndex;
   }
 
   async saveKeyIndex(key, value) {
     let targetFilePath = this.getKeyFilePath(key);
-    let release = await this.lockFile(targetFilePath);
+    await this.lockFile(targetFilePath);
     let result = await this._saveKeyIndexToFile(targetFilePath, key, value);
-    await release();
+    await this.unlockFile(targetFilePath);
     return result;
   }
 
   async loadKeyIndex(key) {
     let targetFilePath = this.getKeyFilePath(key);
-    let release = await this.lockFile(targetFilePath);
+    await this.lockFile(targetFilePath);
     let keyIndex = await this._loadKeyIndexFromFile(targetFilePath, key);
-    await release();
+    await this.unlockFile(targetFilePath);
     return keyIndex;
   }
 
   async deleteKeyIndex(key) {
     let targetFilePath = this.getKeyFilePath(key);
-    let release = await this.lockFile(targetFilePath);
+    await this.lockFile(targetFilePath);
     try {
       await unlink(targetFilePath);
     } catch (error) {
       if (error.code !== 'ENOENT') {
-        await release();
+        await this.unlockFile(targetFilePath);
         throw new Error(
           `Failed to delete the ${key} item from the file system because of error: ${error.message}`
         );
       }
     }
-    await release();
+    await this.unlockFile(targetFilePath);
   }
 
   async _saveKeyIndexToFile(targetFilePath, key, value) {
