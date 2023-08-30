cba7c104f1201d1e29520d54a8e892501090097c
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2023-08-30 22:41:47 +1000

    Add ES module bundle for front end use

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ldpos-client-js
 LDPoS JavaScript client
+
+## Building ES module.js
+
+```
+npm install
+```
+
+Then:
+
+```
+./node_modules/.bin/rollup --config ./rollup.config.js
+```
---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const terser = require('@rollup/plugin-terser');
+const json = require('@rollup/plugin-json');
+const inject = require('@rollup/plugin-inject');
+const nodePolyfills = require('rollup-plugin-node-polyfills');
+
+module.exports = {
+  input: 'esm.js',
+  output: {
+    file: 'module.js',
+    format: 'es'
+  },
+  plugins: [
+    commonjs(),
+    nodePolyfills(),
+    inject({
+      Buffer: ['buffer', 'Buffer']
+    }),
+    resolve({
+      preferBuiltins: false,
+      browser: true
+    }),
+    json(),
+    terser()
+  ]
+};
